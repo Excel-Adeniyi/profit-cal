f2b72a8d4367fa16970bef7b0ead2f11b936231f
excel <adeniyiexcel@gmail.com>
2026-09-22 16:27:02 +0100

    Refactor profit_cal.go to improve error handling and add storeFinancialInfo function

--- profit_cal.go
+++ profit_cal.go
@@ -1,30 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
+func storeFinacialInfo(ebt float64, profit float64, ratio float64) {
+	var data = fmt.Sprintf("EBT: %.1f\nProfif: %.1f\nRatio: %.1f\n", ebt, profit, ratio)
+	os.WriteFile("profit.txt", []byte(data), 0644)
+}
 func main() {
 	// var revenue float64
 	// var expenses float64
 	// var taxRates float64
 
-	revenue := getUserInput("Revenue: ")
-	expenses := getUserInput("Expenses: ")
-	taxRates := getUserInput("Tax Rates: ")
+	revenue, err := getUserInput("Revenue: ")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	expenses, err := getUserInput("Expenses: ")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	taxRates, err := getUserInput("Tax Rates: ")
 
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	ebt, profit, ratio := calFinancials(revenue, expenses, taxRates)
 
 	fmt.Println(ebt)
 	fmt.Println(profit)
 	fmt.Printf("Ratio to 2dp: %.2f\n", ratio)
+	storeFinacialInfo(ebt, profit, ratio)
 }
 
-func getUserInput(infoText string) float64 {
+func getUserInput(infoText string) (float64, error) {
 
 	var userInput float64
 	fmt.Print(infoText)
 	fmt.Scan(&userInput)
-
-	return userInput
+	if userInput <= 0 {
+		return 0, errors.New("Value can't be less or equal to 0")
+	}
+	return userInput, nil
 }
 
 func calFinancials(uRevenue, uExpenses, uTaxRates float64) (ebt, profit, ratio float64) {
